7ebc9d7bced7f47f2a55afe52bf53c4930450055
zhenggao cai <caizhengao1992@gmail.com>
2019-04-19 04:10:03 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,29 @@
+def collect(array_name)
+  i = 0 
+  collection=[]
+  while i < array_name.size 
+  yield array_name[i]
+  collection << array_name[i]
+  i += 1 
+end 
+return collection
+end
+def my_collect(array)
+  new_array=[]
+  keep =[]
+  i = 0
+  while i< array.size 
+  yield array[i]
+  size_array=array[i].split(" ")
+  if size_array.size > 1
+  new_array[i]=array[i].split(" ").first
+  i += 1 
+else new_array[i]=array[i].upcase
+  i += 1 
+  end
+end 
+  return new_array
+  
 
-
+ 
+end 
